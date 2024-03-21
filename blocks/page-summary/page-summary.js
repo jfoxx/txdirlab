4d0dba5446f6908157fc1a25d3d4e87c9346cc3f
Jeff Foxx <foxx@adobe.com>
2024-03-21 16:24:43 -0400

    page summary block

---
+++ blocks/page-summary/page-summary.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+    const title =  document.createElement('h2');
+    title.innerText = "On this page:";
+    block.firstElementChild.prepend(title);
+  }
+  

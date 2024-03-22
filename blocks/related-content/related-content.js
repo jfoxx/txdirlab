43f0129947fa7bda48fcfa83d925994c2a2912b4
Jeff Foxx <foxx@adobe.com>
2024-03-22 12:01:33 -0400

    linting

--- blocks/related-content/related-content.js
+++ blocks/related-content/related-content.js
@@ -1,11 +1,10 @@
 export default function decorate(block) {
-    const title = document.createElement('p');
-    title.innerText = 'Related Content';
-    title.className = 'title';
-    block.firstElementChild.prepend(title);
-    const links = block.querySelectorAll('a');
-    links.forEach(i => {
-        i.classList.remove('button');
-    });
-  }
-  
+  const title = document.createElement('p');
+  title.innerText = 'Related Content';
+  title.className = 'title';
+  block.firstElementChild.prepend(title);
+  const links = block.querySelectorAll('a');
+  links.forEach((i) => {
+    i.classList.remove('button');
+  });
+}
